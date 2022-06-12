264a0db4098c74eda9189f7260bede7627692703
Elvina Kurseitova <el.oskich@gmail.com>
2022-06-12 18:22:43 +0300

    Updates

---
+++ precache-manifest.077aaef4f1542e58ca413e361a3f4c1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63167998ec588386d083",
+    "url": "/static/css/main.916c4347.chunk.css"
+  },
+  {
+    "revision": "63167998ec588386d083",
+    "url": "/static/js/main.63167998.chunk.js"
+  },
+  {
+    "revision": "8f1f9bdd8571ce5e7bb7",
+    "url": "/static/js/1.8f1f9bdd.chunk.js"
+  },
+  {
+    "revision": "e18e91382806567bcbbc",
+    "url": "/static/js/2.e18e9138.chunk.js"
+  },
+  {
+    "revision": "c629b6aa4d20327ca5f7",
+    "url": "/static/js/runtime~main.c629b6aa.js"
+  },
+  {
+    "revision": "bd25b9b15ed9cfdbb26b916fa334ac60",
+    "url": "/static/media/about.bd25b9b1.jpg"
+  },
+  {
+    "revision": "1a7f9cfe23b326643ad8631ccf85de35",
+    "url": "/static/media/logo.1a7f9cfe.svg"
+  },
+  {
+    "revision": "6cc2c7f57a8c53c4f06a9a718d6af56c",
+    "url": "/index.html"
+  }
+];
